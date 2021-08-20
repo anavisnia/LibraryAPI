a4584467699df9ad0ba7cff5d561bec876d2f70c
Ana Višnevskaja <anavisnia@gmail.com>
2021-08-20 16:26:09 +0300

    added mapping profiles

--- Src/Mappings/MappingsProfile.cs
+++ Src/Mappings/MappingsProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using LibraryAPI.Dtos;
+using LibraryAPI.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,13 @@ using System.Threading.Tasks;
 
 namespace LibraryAPI.Mappings
 {
-    public class MappingsProfile
+    public class MappingsProfile : Profile
     {
-        
+        public MappingsProfile()
+        {
+            CreateMap<Author, AuthorDto>().ReverseMap();
+
+            CreateMap<Book, BookDto>().ReverseMap();
+        }
     }
 }
