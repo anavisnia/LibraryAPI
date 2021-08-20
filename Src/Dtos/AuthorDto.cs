f90f2570c1ca1d9803380df60d1f271cbe00e71e
Ana Višnevskaja <anavisnia@gmail.com>
2021-08-20 15:23:06 +0300

    Entities and dtos added

---
+++ Src/Dtos/AuthorDto.cs
@@ -0,0 +1,16 @@
+﻿using LibraryAPI.Dtos.Base;
+using LibraryAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Dtos
+{
+    public class AuthorDto : Dto
+    {
+        public string Surname { get; set; }
+
+        public Book Book { get; set; }
+    }
+}
---
+++ Src/Entities/Base/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAPI.Entities.Base
+{
+    public class Entity
+    {
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? Year { get; set; }
+    }
+}
